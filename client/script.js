8e39d9c3ef529323c7bd8b7ef595b07535f65c88
abc <strelkovv.911@mail.ru>
2021-12-05 15:16:04 +0300

    norm porabotali

--- client/script.js
+++ client/script.js
@@ -1,6 +1,10 @@
-var request = $.get("http://localhost:591")
+var request = $.get("http://localhost:591/quote")
 request.done(function(data){
-    console.log(data);
+    var dict_quote = JSON.parse(data);
+    var title = "Цитата" + dict_quote["number"];
+    var content = dict_quote["quote"]
+    $("title").text(title);
+    $("#text").text(content);
 });
 request.fail(function(jqXHR, textStatus, errorThrown){
     console.log(textStatus, errorThrown);
--- server/index.js
+++ server/index.js
@@ -1,8 +1,18 @@
 var express = require("express")
 var cors = require("cors");
+const { randomInt } = require("crypto");
 var app = express();
 app.use(cors());
-app.get("/", function(request, response){
-    response.send("000");
+app.get("/quote", function(request, response){
+    var fs = require("fs");
+    var fileContent = fs.readFileSync("server/db.txt", "utf8");
+    var strings = fileContent.split("\r\n")
+    var randomNum = randomInt(strings.length)
+    var dict_quote = {
+        number: (randomNum + 1),
+        quote: strings[randomNum]
+    };
+    var data = JSON.stringify(dict_quote);
+    response.send (data);
 });
 app.listen(591);
